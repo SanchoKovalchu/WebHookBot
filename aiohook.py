a63d726ed8ab178eaf517b779e74ec0422fb4b3c
SanchoKovalchu <koworkwd@gmail.com>
2022-05-27 20:21:55 +0300

    Update aiohook.py

--- aiohook.py
+++ aiohook.py
@@ -1,18 +1,10 @@
 import logging
 
-
-
 from aiogram import Bot, types
-
-
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-
-
 from aiogram.dispatcher import Dispatcher
-
+from aiogram.dispatcher.webhook import SendMessage
 from aiogram.utils.executor import start_webhook
-
-
 
 
 API_TOKEN = '5302840148:AAGtGfjfQZWbwRn0mqPrv_rEqRhK9XEiarg'
@@ -31,50 +23,47 @@ WEBAPP_PORT = 8443
 
 logging.basicConfig(level=logging.INFO)
 
-
 bot = Bot(token=API_TOKEN)
-
 dp = Dispatcher(bot)
-
 dp.middleware.setup(LoggingMiddleware())
 
 
-@dp.message_handler(commands="start")
-async def cmd_start(message: types.Message):
-    await message.answer("Ласкаво прошу до StudyBot!")
+@dp.message_handler()
+async def echo(message: types.Message):
+    # Regular request
+    # await bot.send_message(message.chat.id, message.text)
+
+    # or reply INTO webhook
+    return SendMessage(message.chat.id, message.text)
 
 
-
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
 
 
 async def on_shutdown(dp):
-    
     logging.warning('Shutting down..')
 
+    # insert code here to run it before shutdown
 
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
 
-  # insert code here to run it before shutdown
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
 
-  # Remove webhook (not acceptable in some cases)
-
-await bot.delete_webhook()
-
-  # Close DB connection (if used)
-
-await dp.storage.close()
-
-await dp.storage.wait_closed()
-
-
-logging.warning('Bye!')
+    logging.warning('Bye!')
 
 
 if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
-        )
+    )
