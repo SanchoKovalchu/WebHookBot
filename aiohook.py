832ef277d939690bf46b9c641e99c3cc9065c410
SanchoKovalchu <koworkwd@gmail.com>
2022-05-27 20:18:36 +0300

    aiohook

---
+++ aiohook.py
@@ -0,0 +1,80 @@
+import logging
+
+
+
+from aiogram import Bot, types
+
+
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+
+from aiogram.dispatcher import Dispatcher
+
+from aiogram.utils.executor import start_webhook
+
+
+
+
+API_TOKEN = '5302840148:AAGtGfjfQZWbwRn0mqPrv_rEqRhK9XEiarg'
+
+WEBHOOK_HOST = '130.211.226.27'
+
+WEBHOOK_PATH = 8443
+
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+  # webserver settings
+
+WEBAPP_HOST = '0.0.0.0'  # or ip
+
+WEBAPP_PORT = 8443
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=API_TOKEN)
+
+dp = Dispatcher(bot)
+
+dp.middleware.setup(LoggingMiddleware())
+
+
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    await message.answer("Ласкаво прошу до StudyBot!")
+
+
+
+
+
+async def on_shutdown(dp):
+    
+    logging.warning('Shutting down..')
+
+
+
+  # insert code here to run it before shutdown
+
+  # Remove webhook (not acceptable in some cases)
+
+await bot.delete_webhook()
+
+  # Close DB connection (if used)
+
+await dp.storage.close()
+
+await dp.storage.wait_closed()
+
+
+logging.warning('Bye!')
+
+
+if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+        )
