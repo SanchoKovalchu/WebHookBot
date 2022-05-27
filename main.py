3a92cb45303372c1d601f882d61e3294632b3555
SanchoKovalchu <koworkwd@gmail.com>
2022-05-27 16:49:23 +0300

    v2

--- main.py
+++ main.py
@@ -89,8 +89,8 @@ WEBHOOK_HOST = '130.211.226.27'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = './url_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './url_pkey.pem'  # Path to the ssl private key
 
 
 @dp.message_handler(commands="start")
