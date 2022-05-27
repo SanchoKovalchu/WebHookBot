4d3d200548fddf89a55d6c67f944a46ad7f93e8b
SanchoKovalchu <koworkwd@gmail.com>
2022-05-27 20:54:54 +0300

    Update aiohook.py

--- aiohook.py
+++ aiohook.py
@@ -40,8 +40,8 @@ async def echo(message: types.Message):
     return SendMessage(message.chat.id, message.text)
 
 
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
+# async def on_startup(dp):
+#     await bot.set_webhook(WEBHOOK_URL)
     # insert code here to run it after start
 
 
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
+        # on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
